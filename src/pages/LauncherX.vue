9f03f8ab34a1aa257e22f3dc6a0f27952be05534
Feiron <53085628+fr1g@users.noreply.github.com>
2024-05-25 10:09:13 +0800

    你妈就这就过不了编译?

--- src/pages/LauncherX.vue
+++ src/pages/LauncherX.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { VNode, onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import ContactInfo from '../components/ContactInfo.vue';
